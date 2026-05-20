81760c0a61feacf6fc5621dd6de7479f9617f835
hassana412 <hassana412@gmail.com>
2026-05-20 22:16:29 +0100

    structure: add src/shared/const.ts

---
+++ src/shared/const.ts
@@ -0,0 +1,9 @@
+export const COOKIE_NAME = "app_session_id";
+export const ONE_YEAR_MS = 1000 * 60 * 60 * 24 * 365;
+export const AXIOS_TIMEOUT_MS = 30_000;
+export const UNAUTHED_ERR_MSG = 'Please login (10001)';
+export const NOT_ADMIN_ERR_MSG = 'You do not have required permission (10002)';
+
+export const VIP_PRICE_FCFA = 15000;
+export const PLATFORM_FEE_PCT = 0.08; // 8% platform fee
+export const CURRENCY = "FCFA";
